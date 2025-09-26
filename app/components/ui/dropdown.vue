dc62e994a4567480ad4cd9711aa75e107ec6cdc1
GustoBit <azimjoneshmetov2002@gmail.com>
2025-09-26 13:51:55 +0200

    Fixed Drawer Mobile

--- app/components/ui/dropdown.vue
+++ app/components/ui/dropdown.vue
@@ -95,18 +95,37 @@ import { useDrawerStore } from '~/store/ui/drawer'
 const categoryStore = useCategoryStore()
 const { hasSub } = storeToRefs(categoryStore)
 
+const normalizedHasSub = computed(() => {
+	if (!hasSub.value.length) return []
+
+	const reorderedHasSub = [...hasSub.value.slice(1), hasSub.value[0]]
+
+	return reorderedHasSub.map((cat) => {
+		if (!cat?.subs || !cat.subs.length) {
+			return {
+				...cat,
+				subs: [{ slug: cat?.slug, title: cat?.title, _id: cat?._id }],
+			}
+		}
+
+		const [first, ...rest] = cat.subs
+
+		return {
+			...cat,
+			subs: [{ slug: cat.slug, title: cat.title, _id: cat._id }, ...rest, first],
+		}
+	})
+})
+
 const drawerStore = useDrawerStore()
 
-// faqat bitta index ochiq bo‘ladi
 const activeIndex = ref<number | null>(null)
 const contentRefs = ref<HTMLElement[]>([])
 
 const toggle = (index: number): void => {
 	if (activeIndex.value === index) {
-		// agar shu ochiq bo‘lsa → yopiladi
 		activeIndex.value = null
 	} else {
-		// boshqasini yopib yangisini ochamiz
 		activeIndex.value = index
 	}
 
@@ -120,35 +139,31 @@ const toggle = (index: number): void => {
 </script>
 
 <template>
-	<div class="space-y-4">
+	<div class="space-y-2">
 		<div
-			v-for="(item, index) in hasSub"
+			v-for="(item, index) in normalizedHasSub"
 			:key="index"
 			class="flex flex-col"
 		>
-			<div class="grid grid-cols-[2fr_1fr] divide-x-2 dark:divide-gray-400/30 divide-gray-400/20 bg-gray-200 rounded-lg overflow-hidden dark:bg-gray-500/20">
-				<NuxtLink
-					:to="$localePath(`/category/${item.slug}`)"
-					class="text-xl semibold px-2 p-2 flex items-center active:bg-gray-500/20"
-					@click="drawerStore.setOpen()"
-				>
-					<p>{{ item.title }}</p>
-				</NuxtLink>
+			<button
+				:class="['flex items-center justify-between gap-2 p-2 rounded-lg active:bg-gray-500/20', activeIndex == index ? 'dark:bg-gray-500/20 bg-gray-200' : '']"
+				@click="toggle(index)"
+			>
+				<h1 class="text-xl semibold flex items-center">
+					{{ item.title }}
+				</h1>
 
-				<button
-					class="flex items-center justify-end p-2 w-full active:bg-gray-500/20"
-					@click="toggle(index)"
-				>
+				<span class="flex items-center justify-center">
 					<UIcon
 						name="line-md:chevron-small-right"
 						:class="['w-8 h-8 transition duration-300 ease-in-out', activeIndex === index ? 'rotate-90' : '']"
 					/>
-				</button>
-			</div>
+				</span>
+			</button>
 
 			<div
 				ref="contentRefs"
-				:class="['overflow-hidden transition-all duration-300 ease-in-out pl-2 pr-4', activeIndex === index ? 'mt-6' : '']"
+				:class="['overflow-hidden transition-all duration-300 ease-in-out pl-2 pr-4', activeIndex === index ? 'mt-2' : '']"
 				:style="{
 					maxHeight: activeIndex === index ? (contentRefs[index]?.scrollHeight || 0) + 'px' : '0px',
 				}"
@@ -157,12 +172,12 @@ const toggle = (index: number): void => {
 					<div class="flex-1 space-y-2">
 						<NuxtLink
 							v-for="v in item.subs"
-							:key="v._id"
-							:to="$localePath(`/category/${v.slug}`)"
+							:key="v?._id"
+							:to="$localePath(`/category/${v?.slug}`)"
 							class="block px-2 py-1 text-lg medium rounded-lg active:bg-gray-100 dark:active:bg-gray-500/20"
 							@click="drawerStore.setOpen()"
 						>
-							{{ v.title }}
+							{{ v?.title }}
 						</NuxtLink>
 					</div>
 
